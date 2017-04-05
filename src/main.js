76721324c48cdf09ed60eb687fb50767fff0c16f
xun <mx@rmbz.net>
2017-04-05 19:54:03 +0800

    rout

--- src/main.js
+++ src/main.js
@@ -1,14 +1,13 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
-Vue.use(VueRouter);
-
 const Foo = {template: '<div>foo</div>'};
 const Bar = {template: '<div>bar</div>'};
 
 const routes = [
     {path: '/foo', component: Foo},
-    {path: '/bar', component: Bar}
+    {path: '/bar', component: Bar},
+    {path: '/', component: AppIndex}
 ];
 
 const router = new VueRouter({
---
+++ src/router/router.vue
@@ -0,0 +1,11 @@
+<template>
+    <div id="app">
+        <script src="//unpkg.com/vue/dist/vue.js"></script>
+        <script src="//unpkg.com/vue-router/dist/vue-router.js"></script>
+        <p>
+            <router-link to="/user/foo">/user/foo</router-link>
+            <router-link to="/user/bar">/user/bar</router-link>
+        </p>
+        <router-view></router-view>
+    </div>
+</template>
